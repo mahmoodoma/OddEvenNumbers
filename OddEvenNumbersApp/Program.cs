313b04cf1c9f86136dd4ad24eed25a50559ce7a5
mahmoodoma <105951555+mahmoodoma@users.noreply.github.com>
2024-04-19 08:51:19 +0200

    refactor iseven and is odd using expression bodied method

--- OddEvenNumbersApp/Program.cs
+++ OddEvenNumbersApp/Program.cs
@@ -26,21 +26,9 @@ namespace OddEvenNumbersApp
 
         }
 
-        static bool IsEven(int number)
-        {
-            if (number  % 2 == 0)
-                return true;
-            else
-                return false;
-        }
+        static bool IsEven(int number) => number % 2 == 0;
 
-        static bool IsOdd(int number)
-        {
-            if (number % 2 != 0 )
-                return true;
-            else
-                return false;
-        }
+        static bool IsOdd(int number) => !IsEven(number);
 
 
     }
